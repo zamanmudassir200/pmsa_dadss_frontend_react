eac44bbdb0e3569210d1860f345178d2a7008043
zamanmudassir200 <zamanmudassir200@gmail.com>
2026-01-14 15:27:40 +0500

    formatted

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,7 @@ export const useStore = create((set) => ({
   // Authentication
   user: null,
   token: null,
-  
+
   login: (user, token) => set({ user, token }),
   logout: () => set({ user: null, token: null }),
 
@@ -13,67 +13,72 @@ export const useStore = create((set) => ({
   platforms: [],
   tempRows: [],
   editingRow: null,
-  
+
   // Set platforms from API
   setPlatforms: (platforms) => set({ platforms }),
-  
+
   // Temporary rows for adding
   addTempRow: (tempId) =>
     set((state) => ({
-      tempRows: [...state.tempRows, { 
-        tempId, 
-        isTemp: true,
-        pf_id: "",
-        pf_name: "",
-        pf_type: "",
-        pf_squadron: "",
-        pf_status: "",
-        pf_co: "",
-        pf_fuelcap: "",
-        pf_watercap: "",
-        pf_info: ""
-      }]
+      tempRows: [
+        ...state.tempRows,
+        {
+          tempId,
+          isTemp: true,
+          pf_id: "",
+          pf_name: "",
+          pf_type: "",
+          pf_squadron: "",
+          pf_status: "",
+          pf_co: "",
+          pf_fuelcap: "",
+          pf_watercap: "",
+          pf_info: "",
+        },
+      ],
     })),
-    
+
   removeTempRow: (tempId) =>
     set((state) => ({
-      tempRows: state.tempRows.filter((row) => row.tempId !== tempId)
+      tempRows: state.tempRows.filter((row) => row.tempId !== tempId),
     })),
-    
+
   updateTempRow: (tempId, field, value) =>
     set((state) => ({
       tempRows: state.tempRows.map((row) =>
         row.tempId === tempId ? { ...row, [field]: value } : row
-      )
+      ),
     })),
-    
+
   clearTempRows: () => set({ tempRows: [] }),
-  
+
   // Update existing row
   updateRow: (pf_key, field, value) =>
     set((state) => ({
       platforms: state.platforms.map((row) =>
         row.pf_key === pf_key ? { ...row, [field]: value } : row
-      )
+      ),
     })),
-    
+
   // Edit row management
   setEditingRow: (row) => set({ editingRow: row }),
   clearEditingRow: () => set({ editingRow: null }),
-  
+
   // Save editing changes to actual data
   saveEditedRow: (updatedRow) =>
     set((state) => ({
       platforms: state.platforms.map((row) =>
         row.pf_key === updatedRow.pf_key ? updatedRow : row
       ),
-      editingRow: null
+      editingRow: null,
     })),
-    
+
   // Add new platform to actual data
   addPlatform: (newPlatform) =>
     set((state) => ({
       platforms: [newPlatform, ...state.platforms],
-      tempRows: state.tempRows.filter(row => row.tempId !== newPlatform.tempId)
-    }))
+      tempRows: state.tempRows.filter(
+        (row) => row.tempId !== newPlatform.tempId
+      ),
+    })),
 }));
